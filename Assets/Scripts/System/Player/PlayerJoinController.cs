1901015d1e682413804b3978e37ff30b1f484d82
Klim-Klymenko <klim171107@gmail.com>
2024-08-26 14:16:49 +0300

    finished project

---
+++ Assets/Scripts/System/Player/PlayerJoinController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Fusion;
+
+namespace Sample.System
+{
+    public sealed class PlayerJoinController : SimulationBehaviour, IPlayerJoined, IPlayerLeft
+    {
+        private PlayerSpawner _playerSpawner;
+
+        private void Awake()
+        {
+            _playerSpawner = this.GetComponent<PlayerSpawner>();
+        }
+
+        void IPlayerJoined.PlayerJoined(PlayerRef playerRef)
+        {
+            if (!this.Runner.IsServer)
+            {
+                return;
+            }
+
+            _playerSpawner.SpawnPlayer(playerRef);
+        }
+
+        void IPlayerLeft.PlayerLeft(PlayerRef playerRef)
+        {
+            if (!this.Runner.IsServer)
+            {
+                return;
+            }
+
+            NetworkObject playerObject = this.Runner.GetPlayerObject(playerRef);
+            if (playerObject != null)
+            {
+                this.Runner.Despawn(playerObject);
+            }
+        }
+    }
+}
